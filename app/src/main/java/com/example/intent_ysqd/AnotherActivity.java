dc4a16e0ae50534208d77c2ec449b57f1f0ab855
LIU-yuqing <1944026056@qq.com>
2019-11-20 08:46:03 +0800

    ysqd

---
+++ app/src/main/java/com/example/intent_ysqd/AnotherActivity.java
@@ -0,0 +1,12 @@
+package com.example.intent_ysqd;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AnotherActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_another);
+    }
+}
---
+++ app/src/main/java/com/example/intent_ysqd/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.intent_ysqd;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button=(Button)findViewById(R.id.buttonAnother);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent("com.example.intent_ysqd.ACTION_START");
+                intent.addCategory("com.example.intent_ysqd.MY_CATEGORY");
+                startActivity(intent);
+            }
+        });
+    }
+}
